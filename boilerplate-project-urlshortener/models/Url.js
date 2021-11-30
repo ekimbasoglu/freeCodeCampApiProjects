ccbea08eecc4d6f70c0afebf2d75fc5dc378eea1
ekimbasoglu <ekimbasoglu@hotmail.com>
2021-11-30 19:34:45 +0100

    fix(urlshortener): bodyParser fixed, task is done

--- boilerplate-project-urlshortener/models/Url.js
+++ boilerplate-project-urlshortener/models/Url.js
@@ -3,8 +3,8 @@ const { Schema } = mongoose;
 
 const UrlSchema = new mongoose.Schema(
     {
-        number: { type: Number },
-        url: { type: String },
+        short_url: { type: Number },
+        original_url: { type: String },
     }
 );
 
--- boilerplate-project-urlshortener/server.js
+++ boilerplate-project-urlshortener/server.js
@@ -11,12 +11,11 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URL,
+mongoose.connect(process.env.MONGO_DB,
   {
     useNewUrlParser: true,
   }
 );
-
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function (req, res) {
@@ -26,42 +25,29 @@ app.get('/', function (req, res) {
 // Post url
 app.post('/api/shorturl', async (req, res) => {
   const { url } = req.body;
+  console.log(url)
 
-  if (!url) {
-    res.status(401).send({ error: 'Missing url!' })
-  }
+  var regex = new RegExp(/https:\/\//gi);
 
-  var expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
-  var regex = new RegExp(expression);
-
-  if (!url.match(regex)) {
-    res.status(401).send({ error: 'invalid url' });
-  }
-  const number = Math.floor(Math.random() * 100) + 1;
-
-  const checkUrl = await Url.findOne({ number });
-  if (checkUrl) {
-    res.status(401).send({ error: 'invalid url' });
-  };
-
-  if (!checkUrl && url.match(regex)) {
-    await Url.create({ number, url });
-    res.json({ original_url: url, short_url: number });
+  if (url.match(regex)) {
+    let short_url = Math.floor(Math.random() * 100) + 1;
+    console.log("short_url: ", short_url)
+    await Url.create({ short_url, original_url: url });
+    res.json({ original_url: url, short_url });
+  } else {
+    res.json({ error: 'invalid url' });
   }
 
 });
 
-// Get url
-app.get('/api/:id?', async (req, res) => {
-  let param = req.params.id;
-  const url = await Url.findOne({ number: param });
 
-  if (url == 'null') {
-    res.status(400).send({ error: 'There is no url found' })
-  }
+// Get url
+app.get('/api/shorturl/:id?', async (req, res) => {
+  let param = req.params.id;
+  const url = await Url.findOne({ short_url: param });
 
   res.writeHead(302, {
-    location: url.url
+    location: url.original_url
   });
   res.end();
 });
@@ -69,4 +55,3 @@ app.get('/api/:id?', async (req, res) => {
 app.listen(3000, function () {
   console.log(`Listening on port ${port}`);
 });
-
