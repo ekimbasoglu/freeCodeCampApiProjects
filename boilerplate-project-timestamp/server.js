99624914b69fc8e503fac11cbb5825bee9d465b8
Ekim <42123636+ekimbasoglu@users.noreply.github.com>
2021-11-30 10:31:14 +0100

    Initial commit- First timestamp project

---
+++ boilerplate-project-timestamp/server.js
@@ -0,0 +1,48 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+var strftime = require('strftime');
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/:date", async (req, res) => {
+  let param = req.params.date;
+  let date = new Date();
+
+  if (/^\d*$/.test(req.params.date)) {
+    date.setTime(param);
+  } else {
+    date = new Date(param);
+  }
+
+  if (!date.getTime()) {
+    res.status.send({ error: "Invalid date given" })
+  }
+
+  res.send({
+    unix: date.getTime(),
+    utc: strftime('%B %d, %Y', date)
+  });
+
+});
+
+// listen for requests :)
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
